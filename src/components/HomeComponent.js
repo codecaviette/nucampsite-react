8457fe61d11cb87e705ea30104dffa408c757c82
Jenny Chou <jmychou@gmail.com>
2020-06-22 21:16:39 -0700

    Adding to the Single Page App

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
+
+// This builds structure of each featured item card; called in Home functional component below
+function RenderCard({item}) {
+    return (
+        <Card>
+            <CardImg src={item.image} alt={item.name}/>     {/* We're accessing the image and name properties of the argument passed through RenderCard fxn */}
+            <CardBody>
+                <CardTitle>{item.name}</CardTitle>
+                <CardText> {item.description} </CardText>
+            </CardBody>
+        </Card>
+    )
+}
 
 function Home(props) {
     return (
         <div className="container">
-            <h4>Home</h4>
+            <div className="row">
+                <div className="col-md m-1">                {/* When no number succeeds col, all divs in the row will take up same amount of space */}
+                    <RenderCard item={props.campsite} />    {/* Create item prop from passed prop, which is used in RenderCard fxn */}
+                </div>
+                <div className="col-md m-1">
+                    <RenderCard item={props.promotion} />
+                </div>
+                <div className="col-md m-1">
+                    <RenderCard item={props.partner} />
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,9 +3,13 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import { CAMPSITES } from '../shared/campsites';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -13,13 +17,21 @@ class Main extends Component {
         super(props);
         this.state = {
             campsites: CAMPSITES,
+            comments: COMMENTS,
+            partners: PARTNERS,
+            promotions: PROMOTIONS
         };
     }
 
     render() {
-        const HomePage = () => {
+        const HomePage = () => {        // We used arrow function bc they inhert the "this" of their parent scope, so here, we can access state of Main comp. If we'd instead used a fxn declaration, then using "this" keyword would've been undefined
             return (
-                <Home />
+                <Home       
+                    // We want to pass state as the following props to Home comp: featured campsite, featured partner, and featured promo
+                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}        // We'll filter through the campsites array and return the item that has the featured key set to "true". Use [0] to pull the 1st item that meets criteria out of the array.
+                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.state.partners.filter(partner => partner.featured)[0]}
+                />
             );
         }
 
@@ -28,7 +40,9 @@ class Main extends Component {
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
-                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route exact path='/directory' render={() =>                // With Route, whhen passing state as props to a comp, need to use the render syntax
+                        <Directory campsites={this.state.campsites} />} />
+                    <Route exact path='/contactus' component={Contact} />       {/* If not passing any props, no need for render syntax; just use component attribute */}
                     <Redirect to='/home' />                </Switch>
                 <Footer />
             </div>
